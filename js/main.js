801dbf0b20677f310e336d1532d971a9e40a9c3b
kasdu01 <kasdu01@yandex.ru>
2021-10-13 11:12:16 +0300

    Start

---
+++ js/main.js
@@ -0,0 +1,35 @@
+const hotelSwiper = new Swiper('.hotel-slider', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.hotel-slider__button--next',
+    prevEl: '.hotel-slider__button--prev',
+  },
+   keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+    pageUpDown: true,
+  },
+});
+
+const reviewsSlider = new Swiper('.reviews-slider', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.reviews-slider__button--next',
+    prevEl: '.reviews-slider__button--prev',
+  },
+   keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+    pageUpDown: true,
+  },
+   autoHeight: true,
+  });
+  
+
+$('.parallax-window').parallax({imageSrc: '/img/newsletter-bg.jpg'});
